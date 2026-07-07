facb7be8eb768076301f4723c3bba3031f59ed73
Kenny Aragon <kenny.ara.cas.ka@gmail.com>
2026-07-07 14:54:19 -0600

    db hotfix

--- skama-db/seeds/SeedAll.sql
+++ skama-db/seeds/SeedAll.sql
@@ -2,10 +2,29 @@
   SKAMA — Ejecutar todos los seeds de desarrollo en orden.
   Uso con sqlcmd (ajusta servidor y base de datos):
 
-  sqlcmd -S localhost -d SkamaDb -E -i SeedAll.sql
+  PowerShell:
+    Set-Location c:\repos\desarrollo-de-aplicaciones\skama-db\seeds
+    sqlcmd -S localhost -d "skama-db" -E -b -i SeedAll.sql
 
-  Requiere tablas y stored procedures ya creados.
+  CMD:
+    cd /d c:\repos\desarrollo-de-aplicaciones\skama-db\seeds
+    sqlcmd -S localhost -d "skama-db" -E -b -i SeedAll.sql
+
+  Requiere tablas ya creadas.
+  Nota SSMS: habilitar Query > SQLCMD Mode para que funcionen los :r.
 */
+
+:ON ERROR EXIT
+
+IF DB_ID(N'skama-db') IS NULL
+BEGIN
+    RAISERROR('La base de datos [skama-db] no existe en la instancia actual.', 16, 1);
+    RETURN;
+END;
+GO
+
+USE [skama-db];
+GO
 
 :r Role.sql
 :r ResultCode.sql
